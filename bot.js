061f15ea98c2a746c5002aaebb03f294f3ca0536
MotazYT <44732876+MotazYT@users.noreply.github.com>
2019-08-08 21:21:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "608257201508974612"; // ايدي السررفر
-var channel = "608257348422598705";//ايدي الروم
+var server = "551512784542629888"; // ايدي السررفر
+var channel = "551533190838222853";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
